667fb8b9aad7ca6c3efa2fe51db7bac97272b220
Francisco Santamaria <appmanwebs@gmail.com>
2020-10-18 06:47:20 -0500

    Apply images & font Open Sans Condensed to menu-item component.

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './menu-item.styles.scss';
 
-const MenuItem = ( {title, linkUrl, size} ) => (
-   <div className="menu-item">
+const MenuItem = ( {title, imageUrl, size} ) => (
+   <div className="menu-item"
+        style={{ backgroundImage: `url(${imageUrl})`}}
+   >
      <div className="content">
-       <h1>{ title }</h1>
-       <span>SHOP NOW</span>
+       <h1 className="title ">{ title }</h1>
+       <span className="subtitle">SHOP NOW</span>
      </div>
    </div>
 )
