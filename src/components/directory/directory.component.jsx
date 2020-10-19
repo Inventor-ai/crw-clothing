50149a3c06c205919e1a733ecbf5a91884c32aea
Francisco Santamaria <appmanwebs@gmail.com>
2020-10-18 21:01:32 -0500

    Homepage styled and organizated

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,60 +1,67 @@
 import React from 'react';
-import './directory.styles.scss'
-import MenuItem from '../menu-item/menu-item.component';
+import MenuItem from '../menu-item/menu-item.component'
+import './directory.styles.scss';
 
 class Directory extends React.Component {
   constructor () {
-    super();
-    this.state = {
-      // This data source could be get it by an API service
-      sections: [
-        {
-          title: 'hats',
-          imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-          id: 1,
-          linkUrl: 'shop/hats'
-        },
-        {
-          title: 'jackets',
-          imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-          id: 2,
-          linkUrl: 'shop/jackets'
-        },
-        {
-          title: 'sneakers',
-          imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-          id: 3,
-          linkUrl: 'shop/sneakers'
-        },
-        {
-          title: 'womens',
-          imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-          size: 'large',
-          id: 4,
-          linkUrl: 'shop/womens'
-        },
-        {
-          title: 'mens',
-          imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-          size: 'large',
-          id: 5,
-          linkUrl: 'shop/mens'
-        }
-      ]
-    }
+     super();
+     this.state = {
+        // This data source could be replaced it by an API service
+        sections: [
+          {
+            title: 'hats',
+            imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+            id: 1,
+            linkUrl: 'shop/hats'
+          },
+          {
+            title: 'jackets',
+            imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+            id: 2,
+            linkUrl: 'shop/jackets'
+          },
+          {
+            title: 'sneakers',
+            imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+            id: 3,
+            linkUrl: 'shop/sneakers'
+          },
+          {
+            title: 'womens',
+            imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+            size: 'large',
+            id: 4,
+            linkUrl: 'shop/womens'
+          },
+          {
+            title: 'mens',
+            imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+            size: 'large',
+            id: 5,
+            linkUrl: 'shop/mens'
+          }
+        ]
+     }
   }
-  
-  render () {
+
+  render() {
     return (
-      <div className="directory-menu">
+    <div className="directory-menu">
       {
-       this.state.sections.map (({id, title, size, imageUrl}) => (
-        <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>
-       ))
+        // Desestructurando el objeto
+        this.state.sections.map (( {title, imageUrl, id, size } )=> {
+          return <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>
+        })
+/*
+        // Enviando el objeto
+        this.state.sections.map ( ( section )=> {
+          <MenuItem key={section.id} title={ section }/>
+        });
+*/
       }
-      </div>
-    )
-  }
+    </div>
+    );
+  } 
 }
 
 export default Directory;
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -4,7 +4,9 @@ import Directory from '../../components/directory/directory.component';
 import './homepage.styles.scss';
 
 const Homepage = ()=> (
-  <Directory />
+  <div className="homepage">
+    <Directory />
+  </div>
 )
 
 export default Homepage;
